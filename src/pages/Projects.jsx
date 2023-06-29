c754d2c469b736e9156a95dbb6249eb01d852796
Aarya Chopkar <aaryachopkar@gmail.com>
2023-06-29 21:41:06 +0530

    project section started

---
+++ src/pages/Projects.jsx
@@ -0,0 +1,13 @@
+import ProjectCard from '@/components/ProjectCard'
+import React from 'react'
+
+function Projects() {
+  return (
+    <section className='bg-[#111] p-36'>
+        <h1 className='text-[#f1cf29] font-bold text-2xl'>PROJECTS</h1>
+        {/* <ProjectCard title={'YouTube'}/> */}
+    </section>
+  )
+}
+
+export default Projects
