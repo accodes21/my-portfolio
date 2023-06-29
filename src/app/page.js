076c61ffc743c588e2e7d606ea95c327dfaa27d6
Aarya Chopkar <aaryachopkar@gmail.com>
2023-06-29 21:44:36 +0530

    additions in layout

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,7 @@
 import Header from '@/components/Header'
 import Hero from '@/components/Hero'
 import About from '@/pages/About'
+import Projects from '@/pages/Projects'
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <Header/>
       <Hero/>
       <About/>
+      <Projects/>
     </main>
   )
 }
