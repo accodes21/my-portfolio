aea5c4d32beef02c70a3920664f807924dcb6d83
Aarya Chopkar <aaryachopkar@gmail.com>
2023-06-29 21:40:15 +0530

    added content aboutme

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCab } from '@fortawesome/free-solid-svg-icons'
 
 function About() {
   return (
@@ -13,7 +15,9 @@ function About() {
                 <div className="text-side pl-4">
                     <h2 className='text-[#f1cf29] font-bold text-2xl'>ABOUT ME</h2>
                     <h3 className='text-3xl mb-8'>Web Developer based in India</h3>
-                    <p className='text-xl text-gray-300'>As a Junior Front-End Developer, I possess an impressive sets of skills in HTML, CSS, JavaScript, React, Tailwind, and Next. I design and maintain websites that offer a smooth user experience. My expertise lies in creating dynamic, engaging interfaces through writing clean code and utilizing new-age development tools and techniques. I am also a acitve contributor to various Open Source projects. I am member of various Tech Communities including <Link href={'https://github.com/EddieHubCommunity'} className='underline hover:text-[#f1cf29]'>EddieHub</Link>, <Link href={'https://github.com/AskDevs'} className='underline hover:text-[#f1cf29]'>AskDevs</Link>, <Link href={'https://gdg.community.dev/gdg-jammu/'} className='underline hover:text-[#f1cf29]'>GDG Jammu</Link> etc.</p>
+                    <p className='text-xl text-gray-300'>As a Junior Front-End Developer, I possess an impressive sets of skills in HTML, CSS, JavaScript, React, Tailwind, and Next. I design and maintain websites that offer a smooth user experience. My expertise lies in creating dynamic, engaging interfaces through writing clean code and utilizing new-age development tools and techniques. I am also a acitve contributor to various Open Source projects. I am member of various Tech Communities including <Link href={'https://github.com/EddieHubCommunity'} target='_blank' className='underline hover:text-[#f1cf29]'>EddieHub</Link>, <Link href={'https://github.com/AskDevs'} target='_blank' className='underline hover:text-[#f1cf29]'>AskDevs</Link>, <Link href={'https://gdg.community.dev/gdg-jammu/'} target='_blank' className='underline hover:text-[#f1cf29]'>GDG Jammu</Link> etc.
+                    <FontAwesomeIcon icon={["fab", "github"]} />
+                    </p>
                 </div>
             </div>
         </div>
