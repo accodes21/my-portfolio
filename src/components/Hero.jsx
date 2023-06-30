ae4305e2ddc7bdf73482a72570334f0078470d2a
Aarya Chopkar <aaryachopkar@gmail.com>
2023-06-30 16:14:41 +0530

    text alignment readjusted

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,9 +4,9 @@ import Image from 'next/image'
 function Hero() {
   return (
     <section className='bg-[#111]'>
-        <div className="content flex items-center justify-center h-[40rem] gap-40">
+        <div className="content1 flex items-center justify-center h-[40rem] gap-40">
             <div className="hero flex items-center justify-center relative gap-40">
-                <div className="hero-text flex flex-col text-left">
+                <div className="hero-text flex flex-col">
                     <h1 className='text-[4.8rem] leading-4 mb-[2rem] font-serif font-extrabold'> <span className='text-transparent bg-clip-text bg-gradient-to-r from-yellow-300 to-orange-500'>AARYA</span> 
                     <br/> <br/> <br/> <br/> <br/>
                     <span className='text-transparent bg-clip-text bg-gradient-to-r from-red-500 to-purple-600'>CHOPKAR</span></h1>
