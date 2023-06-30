dd483fbfb99760fae252aa248161d51f2a40ee6a
Aarya Chopkar <aaryachopkar@gmail.com>
2023-06-30 14:23:45 +0530

    removed fontawesome

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,8 +1,5 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
-import { config } from '@fortawesome/fontawesome-svg-core'
-import '@fortawesome/fontawesome-svg-core/styles.css'
-config.autoAddCss = false
 
 const inter = Inter({ subsets: ['latin'] })
 
