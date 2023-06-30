0b0d137d5dcae388ce79130face1f8dacb62714e
Aarya Chopkar <aaryachopkar@gmail.com>
2023-06-30 20:09:59 +0530

    Added social media links

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,22 +1,37 @@
 import React from 'react'
 import Image from 'next/image'
+import Link from 'next/link'
+import {BsGithub, BsLinkedin, BsTwitter} from 'react-icons/bs'
+import SkillBadge from './SkillBadge'
 
 function Hero() {
   return (
     <section className='bg-[#111]'>
-        <div className="content1 flex items-center justify-center h-[40rem] gap-40">
+        <div className="content1 flex items-center justify-center h-[35em]">
             <div className="hero flex items-center justify-center relative gap-40">
                 <div className="hero-text flex flex-col">
                     <h1 className='text-[4.8rem] leading-4 mb-[2rem] font-serif font-extrabold'> <span className='text-transparent bg-clip-text bg-gradient-to-r from-yellow-300 to-orange-500'>AARYA</span> 
                     <br/> <br/> <br/> <br/> <br/>
                     <span className='text-transparent bg-clip-text bg-gradient-to-r from-red-500 to-purple-600'>CHOPKAR</span></h1>
-                    <p className='text-[1.2rem] leading-4 text-center mt-4'>Hi, I'm a passionate Front-end React Developer based in India. 📍</p>
+                    <p className='text-[1.2rem] leading-4 text-center mt-4'>Hi, I'm a Front-end Developer who loves to create stuff.</p>
+                    <ul className='flex mt-6'>
+                        <li className='hover:text-[#f1cf29] transition-all'>
+                            <Link href={'https://github.com/accodes21'} target='_blank' className='text-3xl'><BsGithub/></Link>
+                        </li>
+                        <li className='ml-4 hover:text-[#f1cf29] transition-all'>
+                            <Link href={'https://www.linkedin.com/in/aarya-chopkar-581aa2228'} target='_blank' className='text-3xl'><BsLinkedin/></Link>
+                        </li>
+                        <li className='ml-4 hover:text-[#f1cf29] transition-all'>
+                        <Link href={'https://twitter.com/aarya_chopkar'} target='_blank' className='text-3xl'><BsTwitter/></Link>
+                        </li>
+                    </ul>
                 </div>
                 <div className="image">
                     <Image className='imag' src='/pf.png' width={300} height={300} alt='profile'/>
                 </div>
             </div>
         </div>
+        <SkillBadge/>
     </section>
   )
 }
