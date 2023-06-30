fd01a557f1491e7214d4313d94a7bc91041c1e96
Aarya Chopkar <aaryachopkar@gmail.com>
2023-06-30 20:10:28 +0530

    added skill icons

---
+++ src/components/SkillBadge.jsx
@@ -0,0 +1,59 @@
+import React from 'react'
+import { ImHtmlFive } from 'react-icons/im'
+import {FaReact, FaNodeJs, FaGitAlt} from 'react-icons/fa'
+import {SiCss3, SiJavascript, SiTailwindcss, SiNextdotjs, SiVite, SiFirebase, SiFigma, SiPython, SiC, SiCplusplus} from 'react-icons/si'
+import {TbBrandVscode} from 'react-icons/tb'
+
+function SkillBadge() {
+  return (
+    <div className='flex pl-20 pr-20 pb-20 justify-center'>
+        <h2 className='text-[#f1cf29] font-bold text-3xl'>Tech Stack  |</h2>
+        <ul className='flex'>
+            <li className='ml-4 hover:text-[#f1cf29] transition-all'>
+                <ImHtmlFive className='text-4xl'/>
+            </li>
+            <li className='ml-4 hover:text-[#f1cf29] transition-all'>
+                <SiCss3 className='text-4xl'/>
+            </li>
+            <li className='ml-4 hover:text-[#f1cf29] transition-all'>
+                <SiJavascript className='text-4xl'/>
+            </li>
+            <li className='ml-4 hover:text-[#f1cf29] transition-all'>
+                <FaReact className='text-4xl'/>
+            </li>
+            <li className='ml-4 hover:text-[#f1cf29] transition-all'>
+                <SiTailwindcss className='text-4xl'/>
+            </li>
+            <li className='ml-4 hover:text-[#f1cf29] transition-all'>
+                <SiNextdotjs className='text-4xl'/>
+            </li>
+            <li className='ml-4 hover:text-[#f1cf29] transition-all'>
+                <SiVite className='text-4xl'/>
+            </li>
+            <li className='ml-4 hover:text-[#f1cf29] transition-all'>
+                <SiFirebase className='text-4xl'/>
+            </li>
+            <li className='ml-4 hover:text-[#f1cf29] transition-all'>
+                <FaNodeJs className='text-4xl'/>
+            </li>
+            <li className='ml-4 hover:text-[#f1cf29] transition-all'>
+                <SiFigma className='text-4xl'/>
+            </li>
+            <li className='ml-4 hover:text-[#f1cf29] transition-all'>
+                <FaGitAlt className='text-4xl'/>
+            </li>
+            <li className='ml-4 hover:text-[#f1cf29] transition-all'>
+                <TbBrandVscode className='text-4xl'/>
+            </li>
+            <li className='ml-4 hover:text-[#f1cf29] transition-all'>
+                <SiPython className='text-4xl'/>
+            </li>
+            <li className='ml-4 hover:text-[#f1cf29] transition-all'>
+                <SiCplusplus className='text-4xl'/>
+            </li>
+        </ul>
+    </div>
+  )
+}
+
+export default SkillBadge
