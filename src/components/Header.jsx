6f1a612dd7718d6493662519f05606be97e666d8
Aarya Chopkar <aaryachopkar@gmail.com>
2023-07-01 15:42:06 +0530

    links updated

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,16 @@
 import Link from 'next/link'
 import React from 'react'
+import Image from 'next/image'
 
 function Header() {
   return (
-    <header className='h-[70px] w-full shadow shadow-gray-800 p-5 sticky'>
+    <header className='h-[4.5em] w-full shadow shadow-gray-600 p-5 sticky'>
         <nav className='flex justify-between font-medium'>
-            <p className='pl-4 text-xl'>Aarya</p>
+            <Image src={'/logo.png'} alt='Logo' width={95} height={95}/>
             <ul className='flex justify-end pr-4 mr-12 text-lg'>
                 <Link href={'/'} className='ml-4 text-white hover:text-yellow-400 transition-all'>Home</Link>
-                <Link href={'/'} className='ml-4 text-white hover:text-yellow-400 transition-all'>About</Link>
-                <Link href={'/'} className='ml-4 text-white hover:text-yellow-400 transition-all'>Projects</Link>
+                <Link href={'#about'} className='ml-4 text-white hover:text-yellow-400 transition-all'>About</Link>
+                <Link href={'#projects'} scroll={false} className='ml-4 text-white hover:text-yellow-400 transition-all'>Projects</Link>
                 <Link href={'/'} className='ml-4 text-white hover:text-yellow-400 transition-all'>Contact</Link>
             </ul>
         </nav>
