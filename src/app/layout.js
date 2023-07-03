ccfb996ad8e4b566700d6e99129bfb0c9b79ed2a
Aarya Chopkar <aaryachopkar@gmail.com>
2023-07-03 21:37:29 +0530

    favicon issues

--- src/app/layout.js
+++ src/app/layout.js
@@ -12,7 +12,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/pf.png" type="image/png" sizes='any' />
+        <link rel="icon" type="image/png" sizes="16x16" href="/logo.png"/>
       </head>
       <body className={inter.className}>{children}</body>
     </html>
