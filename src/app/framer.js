b1b77b1ab8c6697db3434ae7a00e9f242c7c026e
Aarya Chopkar <aaryachopkar@gmail.com>
2023-07-03 21:36:17 +0530

    animations updated

--- src/app/framer.js
+++ src/app/framer.js
@@ -37,65 +37,22 @@ export const fadeAnimation = {
   },
 };
 
-export const headTextAnimation = {
-  initial: { x: 100, opacity: 0 },
-  animate: { x: 0, opacity: 1 },
-  transition: {
-    type: "spring",
-    damping: 5,
-    stiffness: 40,
-    restDelta: 0.001,
-    duration: 0.3,
-  },
-};
-
-export const headContentAnimation = {
-  initial: { y: 100, opacity: 0 },
-  animate: { y: 0, opacity: 1 },
-  transition: {
-    type: "spring",
-    damping: 7,
-    stiffness: 30,
-    restDelta: 0.001,
-    duration: 0.6,
-    delay: 0.2,
-    delayChildren: 0.2,
-  },
-};
-
-export const headContainerAnimation = {
-  initial: { x: -100, opacity: 0, transition: { ...transition, delay: 0.5 } },
-  animate: { x: 0, opacity: 1, transition: { ...transition, delay: 0 } },
-  exit: { x: -100, opacity: 0, transition: { ...transition, delay: 0 } },
-};
-
- export const introHeaderVariants = {
-  hide: {
+ export const introHeaderVariants = (direction) => {
+  return {
+    hide: {
       opacity: 0,
-      x: -200,
-  },
-  show: {
+      x: direction === "left" ? -200 : direction === "right" ? 200 : 0,
+      y: direction === "up" ? 200 : direction === "down" ? -200 : 0,
+    },
+    show: {
       opacity: 1,
       x: 0,
-      transition: {
-        ...transition,
-        duration:0.7
-      },
-  },
-};
-
-export const introPictureVariants = {
-  hide: {
-      opacity: 0,
-      x: 200,
-  },
-  show: {
-      opacity: 1,
-      x: 0,
+      y:0,
       transition: {
         ...transition,
         duration:0.7,
         delay:0.2
       },
-  },
-};
+    },
+  };
+}
