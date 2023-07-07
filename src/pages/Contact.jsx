88b71c982670cfd859a5ac40677a4bbcf77fdc92
Aarya Chopkar <aaryachopkar@gmail.com>
2023-07-07 14:24:57 +0530

    media queries for contact

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -22,7 +22,7 @@ function Contact() {
     <AnimatePresence>
     <motion.section id='contact' className='contact bg-[#000] p-32'>
         <motion.h1 initial='hide' whileInView='show' exit='hide' variants={...introHeaderVariants('left')} className='text-[#f1cf29] font-bold text-2xl'>CONTACT ME</motion.h1>
-        <div className="container mt-4 md:mt-32 px-6 mx-auto lg:w-2/3">
+        <div className="contact-div container mt-4 md:mt-32 px-6 mx-auto lg:w-2/3">
             <div className='container text-slate-800 md:px-12 relative'>
                 <div className='block rounded-xl mt-[-100px] bg-[#111] shadow-xl py-10 md:py-12 px-4 md:px-6'>
                     <div>
