fb7bf93e5c539d5c4873374b6e502ac5938cf249
Aarya Chopkar <aaryachopkar@gmail.com>
2023-08-30 10:29:40 +0530

    metadata added

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,6 +11,15 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <meta name='keywords' content='Code, developer, JavaScript, typescript, web, web developer, web development, coding, developing, next, HTML, CSS, tailwind' />
+        <meta property='og:title' content='Aarya Chopkar' />
+        <meta property='og:description' content='Web Developer working on NextJS. Open Source buff who also loves python.' />
+        <meta property='og:url' content='https://aaryachopkar.vercel.app/' />
+        <meta property='og:type' content='website' />
+        <meta name="twitter:card" content="summary" />
+        <meta property='og:image' content='https://github.com/accodes21/my-portfolio/blob/master/preview.png' />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   )
