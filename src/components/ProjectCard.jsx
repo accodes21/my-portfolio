d700d29dc795672666a2f4c1d8be01de6a2cca76
Aarya Chopkar <aaryachopkar@gmail.com>
2023-09-19 09:43:00 +0530

    project link added

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import Image from 'next/image'
-import {BsGithub} from 'react-icons/bs'
+import {BsGithub, BsLink} from 'react-icons/bs'
 import Link from 'next/link'
 import { AnimatePresence, motion } from 'framer-motion'
 import { introHeaderVariants } from '@/app/framer'
 
-const ProjectCard = ({title, image, link}) => {
+const ProjectCard = ({title, image, link, projectLink}) => {
   return (
     <AnimatePresence>
     <motion.div initial='hide' whileInView='show' exit='hide' variants={...introHeaderVariants('up')} className='card m-4'>
@@ -13,9 +13,12 @@ const ProjectCard = ({title, image, link}) => {
         <div className="content h-full w-full shadow-md shadow-[#585757]">
             <Image src={`/${image}`} alt='YouTube 2.0' width={600} height={540} className='thumbnail'/>
             <div className="faded">
-                <div className="title">
+                <div className="title flex md:flex-row flex-col gap-2">
                     <Link className='link' target="_blank" href={`https://github.com/accodes21/${link}`}>
                         <BsGithub/>
+                    </Link>
+                    <Link className='link' target='_blank' href={`${projectLink}`}>
+                      <BsLink/>
                     </Link>
                 </div>
             </div>
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -9,13 +9,13 @@ const Projects = () => {
     <motion.section id='projects' className='project bg-[#111] p-32'>
         <motion.h1 initial='hide' whileInView='show' exit='hide' variants={...introHeaderVariants('left')} className='text-[#f1cf29] font-bold text-2xl'>PROJECTS</motion.h1>
         <div className="box grid grid-cols-2 justify-center items-center">
-          <ProjectCard title={'YouTube 2.0'} image={'yt.png'} link={'YouTube-Clone'}/>
-          <ProjectCard title={'3D Tshirt Designer'} image={'tshirt.png'} link={'tshirt3d'}/>
-          <ProjectCard title={'Movie-Mania'} image={'movie-mania.png'} link={'Movie-Mania'}/>
-          <ProjectCard title={'Meme Generator'} image={'meme.png'} link={'Meme_Generator'}/>
-          <ProjectCard title={'Tenzies Game'} image={'tenzies.png'} link={'Tenzies-Game'}/>
-          <ProjectCard title={'Sherlock Holmes Landing Page'} image={'sherlock.png'} link={'Sherlock_Landing_Page'}/>
-          <ProjectCard title={'Firebase ChatApp'} image={'chatapp.png'} link={'basic_chatapp'}/>
+          <ProjectCard title={'YouTube 2.0'} image={'yt.png'} link={'YouTube-Clone'} projectLink={'https://youtube-clone-accodes21.vercel.app/'}/>
+          <ProjectCard title={'3D Tshirt Designer'} image={'tshirt.png'} link={'tshirt3d'} projectLink={'https://tshirt3d.vercel.app/'}/>
+          <ProjectCard title={'Movie-Mania'} image={'movie-mania.png'} link={'Movie-Mania'} projectLink={'https://movie-app-bice-ten.vercel.app/'}/>
+          <ProjectCard title={'Meme Generator'} image={'meme.png'} link={'Meme_Generator'} projectLink={'https://meme-generator-accodes21.vercel.app/'}/>
+          <ProjectCard title={'Tenzies Game'} image={'tenzies.png'} link={'Tenzies-Game'} projectLink={'https://tenzies-game-neon.vercel.app/'}/>
+          <ProjectCard title={'Sherlock Holmes Landing Page'} image={'sherlock.png'} link={'Sherlock_Landing_Page'} projectLink={'https://accodes21.github.io/Sherlock_Landing_Page/'}/>
+          <ProjectCard title={'Firebase ChatApp'} image={'chatapp.png'} link={'basic_chatapp'} projectLink={'https://basic-chatapp.vercel.app/'}/>
         </div>
     </motion.section>
     </AnimatePresence>
