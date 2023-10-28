d973457aad7d8dd89292ac139b5c79813eef323a
Aarya Chopkar <aaryachopkar@gmail.com>
2023-10-28 12:02:44 +0530

    updated hover effect using tilt

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -2,16 +2,27 @@ import React from 'react'
 import Image from 'next/image'
 import {BsGithub, BsLink} from 'react-icons/bs'
 import Link from 'next/link'
-import { AnimatePresence, motion } from 'framer-motion'
+import { AnimatePresence, motion} from 'framer-motion'
 import { introHeaderVariants } from '@/app/framer'
+import Tilt from 'react-parallax-tilt'
 
 const ProjectCard = ({title, image, link, projectLink}) => {
   return (
     <AnimatePresence>
-    <motion.div initial="hide" whileInView="show" viewport={{ once: true }} variants={...introHeaderVariants('up')} className='card m-4'>
+    <Tilt
+      glareEnable={true}
+      glareMaxOpacity={0.25}
+      tiltMaxAngleX={10}
+      tiltMaxAngleY={10}
+      perspective={1000}
+      scale={1.05}
+      transitionSpeed={1000}
+      transitionEasing="cubic-bezier(0.19, 1.0, 0.22, 1.0)"
+    >
+    <motion.div initial="hide" whileInView="show" viewport={{ once: true }} variants={...introHeaderVariants('up')} className='card m-4 p-2 bg-gray-950 rounded-xl'>
       <h2 className='text-xl font-bold mb-3'>{title}</h2>
-        <div className="content h-full w-full shadow-md shadow-[#585757]">
-            <Image src={`/${image}`} alt='YouTube 2.0' width={600} height={540} className='thumbnail'/>
+        <div className="content h-full w-full rounded-xl md:rounded-3xl">
+            <Image src={`/${image}`} alt='YouTube 2.0' width={500} height={440} className='thumbnail rounded-xl md:rounded-3xl'/>
             <div className="faded">
                 <div className="title flex md:flex-row flex-col gap-2">
                     <Link className='link' target="_blank" href={`https://github.com/accodes21/${link}`}>
@@ -24,6 +35,7 @@ const ProjectCard = ({title, image, link, projectLink}) => {
             </div>
         </div>
     </motion.div>
+    </Tilt>
     </AnimatePresence>
   )
 }
