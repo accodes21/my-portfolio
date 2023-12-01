917a9806d77cb8e63ae50bd137acf666e4d241be
Aarya Chopkar <aaryachopkar@gmail.com>
2023-12-01 14:35:55 +0530

    added more project btn

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -2,6 +2,7 @@ import ProjectCard from '@/components/ProjectCard'
 import React from 'react'
 import { AnimatePresence, motion } from 'framer-motion'
 import { introHeaderVariants } from '@/app/framer'
+import Link from 'next/link'
 
 const Projects = () => {
   return (
@@ -17,6 +18,9 @@ const Projects = () => {
           <ProjectCard title={'Tenzies Game'} image={'tenzies.png'} link={'Tenzies-Game'} projectLink={'https://tenzies-game-neon.vercel.app/'} skills={'ReactJS, CSS'}/>
           <ProjectCard title={'Sherlock Holmes Landing Page'} image={'sherlock.png'} link={'Sherlock_Landing_Page'} projectLink={'https://accodes21.github.io/Sherlock_Landing_Page/'} skills={'CSS, JavaScript'}/>
           <ProjectCard title={'Firebase ChatApp'} image={'chatapp.png'} link={'basic_chatapp'} projectLink={'https://basic-chatapp.vercel.app/'} skills={'ReactJS, Firebase, Tailwind CSS'}/>
+        </div>
+        <div className='text-center mt-6'>
+          <Link href={"https://github.com/accodes21?tab=repositories"} target='_blank' className='text-center text-lg p-4 bg-black rounded-full border border-[#f1cf29]/60 hover:bg-[#f1cf29] hover:text-black transition-all'>More Projects</Link>
         </div>
     </motion.section>
     </AnimatePresence>
