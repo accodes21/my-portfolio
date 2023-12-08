2227e245cc8fe2fe5d5f3eeec54e012c558567da
Aarya Chopkar <aaryachopkar@gmail.com>
2023-12-08 11:52:32 +0530

    updated margins

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
   return (
     <AnimatePresence>
     <motion.footer {...slideAnimation('up')} className='w-full text-center bg-[#111]'>
-        <p className='py-2'>Copyright &copy; Aarya Chopkar 2023</p>
+        <p className='py-3'>&copy; 2023 Aarya Chopkar. All rights reserved.</p>
     </motion.footer>
     </AnimatePresence>
   )
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,7 +7,7 @@ const Header = () => {
   return (
     <AnimatePresence>
     <motion.header {...slideAnimation('down')} className='w-full'>
-        <nav className='fixed z-10 cursor-pointer flex-row items-center  overflow-hidden rounded-full border px-1 py-4 shadow-xl backdrop-blur-sm backdrop-filter border-[#f1cf29]/60 bg-black bg-opacity-60 lg:left-1/2 lg:flex lg:-translate-x-1/2 lg:transform mt-6 top-8"'>
+        <nav className='fixed z-10 cursor-pointer flex-row items-center  overflow-hidden rounded-full border px-1 py-4 shadow-xl backdrop-blur-sm backdrop-filter border-[#f1cf29]/60 bg-black bg-opacity-60 lg:left-1/2 lg:flex lg:-translate-x-1/2 lg:transform mt-2 top-8"'>
             <motion.ul {...introHeaderVariants('left')} className='flex justify-end pr-4 text-lg'>
                 <Link href={'/'} className='ml-4 text-white hover:text-yellow-400 transition-all'>Home</Link>
                 <Link href={'#about'} className='ml-4 text-white hover:text-yellow-400 transition-all'>About</Link>
