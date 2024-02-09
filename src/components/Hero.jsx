0f2962c3a6e94c373422e3bfecea6c524bb0bc0d
Aarya Chopkar <aaryachopkar@gmail.com>
2024-02-09 18:56:32 +0530

    updated mobile margins

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -16,16 +16,16 @@ const Hero = () => {
                     <h1 className=' heading text-[4.8rem] leading-4 mb-[2rem] font-serif font-extrabold'> <span className='text-transparent bg-clip-text bg-gradient-to-r from-yellow-300 to-orange-500'>AARYA</span> 
                     <br/> <br/> <br/> <br/> <br/>
                     <span className='text-transparent bg-clip-text bg-gradient-to-r from-red-500 to-purple-600'>CHOPKAR</span></h1>
-                    <p className='text-[1.2rem] leading-4 text-center mt-4 italic'>Hi, I'm a Software Developer who loves to create stuff.</p>
+                    <p className='text-[1.2rem] leading-4 text-center mt-4 italic'>Hi, I'm a <span className='font-semibold'>Software Developer</span> who loves to create stuff.</p>
                     <div className='flex flex-col'>
                         <ul className='social flex items-center mt-6 mb-4'>
                             <li className='hover:text-[#f1cf29] hover:rotate-12 transition-all'>
                                 <Link href={'https://github.com/accodes21'} target='_blank' title='GitHub' className='text-3xl'><BsGithub/></Link>
                             </li>
-                            <li className='ml-4 hover:text-[#f1cf29] hover:rotate-12 transition-all'>
+                            <li className='social-li ml-4 hover:text-[#f1cf29] hover:rotate-12 transition-all'>
                                 <Link href={'https://www.linkedin.com/in/aarya-chopkar-581aa2228'} target='_blank' title='LinkedIn' className='text-3xl'><BsLinkedin/></Link>
                             </li>
-                            <li className='ml-4 hover:text-[#f1cf29] hover:rotate-12 transition-all'>
+                            <li className='social-li ml-4 hover:text-[#f1cf29] hover:rotate-12 transition-all'>
                             <Link href={'https://twitter.com/ChopkarAarya'} target='_blank' title='Twitter' className=' text-4xl'>𝕏</Link>
                             </li>
                         </ul>
