7f9530f21c2ba59c6c70ac29eaae79a342725195
Aarya Chopkar <aaryachopkar@gmail.com>
2024-07-05 22:40:30 +0530

    about sec updated

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,15 +4,42 @@ import {motion, AnimatePresence} from 'framer-motion'
 import { introHeaderVariants, slideAnimation } from '@/app/framer'
 
 const Header = () => {
+
+  const links = [
+    {
+      href: "/",
+      label: "Home"
+    },
+    {
+      href: "#about",
+      label: "About"
+    },
+    {
+      href: "#projects",
+      label: "Projects"
+    },
+    {
+      href: "#contact",
+      label: "Contact"
+    }
+  ]
+
   return (
     <AnimatePresence>
     <motion.header {...slideAnimation('down')} className='w-full'>
         <nav className='fixed z-50 cursor-pointer flex-row items-center  overflow-hidden rounded-full border px-1 py-4 shadow-xl backdrop-blur-sm backdrop-filter border-[#f1cf29]/60 bg-black bg-opacity-60 lg:left-1/2 lg:flex lg:-translate-x-1/2 lg:transform mt-4 top-8"'>
             <motion.ul {...introHeaderVariants('left')} className='flex justify-end pr-4 text-lg'>
-                <Link href={'/'} className='ml-4 text-white hover:text-yellow-400 transition-all'>Home</Link>
-                <Link href={'#about'} className='ml-4 text-white hover:text-yellow-400 transition-all'>About</Link>
-                <Link href={'#projects'} scroll={false} className='ml-4 text-white hover:text-yellow-400 transition-all'>Projects</Link>
-                <Link href={'#contact'} className='ml-4 text-white hover:text-yellow-400 transition-all'>Contact</Link>
+              {
+                links?.map((link, index) => (
+                  <Link 
+                  key={index}
+                  href={link.href}
+                  className='ml-4 text-white hover:text-yellow-400 transition-colors'
+                  >
+                    {link.label}
+                  </Link>
+                ))
+              }
             </motion.ul>
         </nav>
     </motion.header>
