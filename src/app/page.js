76f9e703bb2c11947b6d1b98851e36cf11919997
Aarya Chopkar <aaryachopkar@gmail.com>
2024-08-11 11:59:14 +0530

    eslint changes

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,14 @@
 "use client";
-import Footer from '@/components/Footer';
-import Header from '@/components/Header'
-import Hero from '@/components/Hero'
-import About from '@/pages/About'
-import Contact from '@/pages/Contact';
-import Projects from '@/pages/Projects'
-import { useEffect, useRef } from 'react'
+import Footer from "@/components/Footer";
+import FractalTree from "@/components/FractalTree";
+import Header from "@/components/Header";
+import Hero from "@/components/Hero";
+import About from "@/pages/About";
+import Contact from "@/pages/Contact";
+import Projects from "@/pages/Projects";
+import { useEffect, useRef } from "react";
 
 export default function Home() {
-
   const loader = useRef(null);
   const path = useRef(null);
   const initialCurve = 250;
@@ -16,60 +16,67 @@ export default function Home() {
   let start;
 
   useEffect(() => {
-    setPath(initialCurve)
-    setTimeout( () => {
-      requestAnimationFrame(animate)
-    }, 115)
-  },[])
+    setPath(initialCurve);
+    setTimeout(() => {
+      requestAnimationFrame(animate);
+    }, 115);
+  }, []);
 
   const animate = (timestamp) => {
-    if(start === undefined){
-      start = timestamp
+    if (start === undefined) {
+      start = timestamp;
     }
     const elapsed = timestamp - start;
-    loader.current.style.top = easeOutQuad(elapsed, 0, -loaderHeight(), duration) + "px"
+    loader.current.style.top =
+      easeOutQuad(elapsed, 0, -loaderHeight(), duration) + "px";
 
-    if(elapsed < duration){
-      requestAnimationFrame(animate)
+    if (elapsed < duration) {
+      requestAnimationFrame(animate);
     }
-  }
+  };
 
   const easeOutQuad = (time, start, end, duration) => {
     return -end * (time /= duration) * (time - 2) + start;
-  }
+  };
 
   const loaderHeight = () => {
     const loaderBounds = loader.current.getBoundingClientRect();
     return loaderBounds.height;
-  }
+  };
 
   const setPath = (curve) => {
-    const width = window.innerWidth
+    const width = window.innerWidth;
     const height = loaderHeight();
-    path.current.setAttributeNS(null, "d",
-    `M0 0
+    path.current.setAttributeNS(
+      null,
+      "d",
+      `M0 0
     L${width} 0
     L${width} ${height}
-    Q${width/2} ${height - curve} 0 ${height}
+    Q${width / 2} ${height - curve} 0 ${height}
     L0 0`
-    )
-  }
+    );
+  };
 
   return (
     <>
       <div ref={loader} className="loader w-full fixed z-10">
-        <svg className='h-full w-full'>
-          <path ref={path} className='stroke-[#f1cf29] stroke-1 fill-[#f1cf29]'></path>
+        <svg className="h-full w-full">
+          <path
+            ref={path}
+            className="stroke-[#f1cf29] stroke-1 fill-[#f1cf29]"
+          ></path>
         </svg>
       </div>
-      <Header/>
-    <main className='h-full w-full'>
-      <Hero/>
-      <About/>
-      <Projects/>
-      <Contact/>
-      <Footer/>
-    </main>
+      <Header />
+      <main className="h-full w-full">
+        <Hero />
+        <About />
+        <Projects />
+        <Contact />
+        {/* <FractalTree /> */}
+        <Footer />
+      </main>
     </>
-  )
+  );
 }
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,15 +1,25 @@
-import { slideAnimation } from '@/app/framer'
-import { AnimatePresence, motion } from 'framer-motion'
-import React from 'react'
+import { slideAnimation, introHeaderVariants } from "@/app/framer";
+import { AnimatePresence, motion } from "framer-motion";
+import React from "react";
 
 const Footer = () => {
   return (
     <AnimatePresence>
-    <motion.footer {...slideAnimation('up')} className='w-full text-center bg-[#111]'>
-        <p className='py-3'>&copy; 2023 Aarya Chopkar. All rights reserved.</p>
-    </motion.footer>
+      <motion.hr
+        initial="hide"
+        whileInView="show"
+        viewport={{ once: true }}
+        variants={introHeaderVariants("left")}
+        className="lg:h-px h-[1.8px] border-t-0 bg-gradient-to-r from-black via-[#f1cf29] to-black opacity-100"
+      />
+      <motion.footer
+        {...slideAnimation("up")}
+        className="w-full text-center bg-[#111]"
+      >
+        <p className="py-3">&copy; 2023 Aarya Chopkar. All rights reserved.</p>
+      </motion.footer>
     </AnimatePresence>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
