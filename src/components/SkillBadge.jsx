ff24da119aaa09b9820705ad00b08b729642f270
Aarya Chopkar <aaryachopkar@gmail.com>
2024-09-08 22:42:28 +0530

    fix: whileInView for skillbadge

--- src/components/SkillBadge.jsx
+++ src/components/SkillBadge.jsx
@@ -1,35 +1,38 @@
 import React from "react";
-import { motion, AnimatePresence } from "framer-motion";
+import { motion, AnimatePresence, useInView } from "framer-motion";
 // import { slideAnimation, skillStagerVariants } from "@/app/framer";
 import skills from "@/data/skills";
 
-const containerVariants = {
-  hidden: { opacity: 0, y: -50 },
-  show: {
-    opacity: 1,
-    y: 0,
-    transition: {
-      staggerChildren: 0.15,
-      delay: 0.2,
-    },
-  },
-};
-
-const skillVariants = {
-  hidden: { opacity: 0, y: -50 },
-  show: {
-    opacity: 1,
-    y: 0,
-  },
-};
-
 const SkillBadge = () => {
+  const ref = React.useRef(null);
+  const isInView = useInView(ref, { once: true });
+
+  const containerVariants = {
+    hidden: { opacity: 0, y: -50 },
+    show: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        staggerChildren: 0.15,
+        delay: 0.2,
+      },
+    },
+  };
+
+  const skillVariants = {
+    hidden: { opacity: 0, y: -50 },
+    show: {
+      opacity: 1,
+      y: 0,
+    },
+  };
+
   return (
     <AnimatePresence>
       <motion.div
         variants={containerVariants}
         initial="hidden"
-        animate="show"
+        animate={isInView ? "show" : "hidden"}
         className="skill-container flex pl-16 pr-16 pb-16 justify-center"
       >
         <h2 className="heading2 text-[#f1cf29] font-bold text-3xl">
@@ -38,6 +41,7 @@ const SkillBadge = () => {
         <ul className="skill-list flex items-center">
           {skills?.map((skill, index) => (
             <motion.li
+              ref={ref}
               variants={skillVariants}
               key={index}
               className={`ml-4 transition-colors cursor-pointer`}
