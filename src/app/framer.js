290530351b6b06eb29e0a9ef553a5f32a70a21ef
Aarya Chopkar <aaryachopkar@gmail.com>
2024-10-13 23:04:05 +0530

    feat: framer-motion blur effect

--- src/app/framer.js
+++ src/app/framer.js
@@ -40,42 +40,26 @@ export const fadeAnimation = {
 export const introHeaderVariants = (direction) => {
   return {
     hide: {
+      filter: "blur(10px)",
       opacity: 0,
       x: direction === "left" ? -200 : direction === "right" ? 200 : 0,
       y: direction === "up" ? 200 : direction === "down" ? -200 : 0,
+      transition: {
+        ...transition,
+        duration: 0.8,
+      },
     },
     show: {
+      filter: "blur(0px)",
       opacity: 1,
       x: 0,
       y: 0,
       transition: {
         ...transition,
-        duration: 0.7,
+        duration: 0.8,
         delay: 0.2,
+        ease: "easeInOut",
       },
     },
   };
-};
-
-export const projectVariants = {
-  hide: {
-    filter: "blur(10px)",
-    y: 200,
-    opacity: 0,
-    transition: {
-      ...transition,
-      duration: 1,
-    },
-  },
-  show: {
-    filter: "blur(0px)",
-    y: 0,
-    opacity: 1,
-    transition: {
-      ...transition,
-      duration: 1,
-      delay: 0.2,
-      ease: "easeInOut",
-    },
-  },
 };
