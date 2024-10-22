b5cd961730d357f04c217e112da486731cb007a1
Aarya Chopkar <aaryachopkar@gmail.com>
2024-10-22 12:40:01 +0530

    feat: lenis smooth scroll

--- src/app/page.js
+++ src/app/page.js
@@ -1,72 +1,28 @@
 "use client";
-import Footer from "@/components/Footer";
-import Header from "@/components/Header";
-import Hero from "@/components/Hero";
-import About from "@/pages/About";
-import Contact from "@/pages/Contact";
-import Projects from "@/pages/Projects";
+import { Footer, Header, Hero, Loader } from "@/components/exports";
+import { About, Contact, Projects } from "@/pages/exports";
 import { useEffect, useRef } from "react";
+import Lenis from "@studio-freight/lenis";
 
 export default function Home() {
   const loader = useRef(null);
   const path = useRef(null);
-  const initialCurve = 250;
-  const duration = 300;
-  let start;
 
   useEffect(() => {
-    setPath(initialCurve);
-    setTimeout(() => {
-      requestAnimationFrame(animate);
-    }, 115);
-  }, []);
+    const lenis = new Lenis();
 
-  const animate = (timestamp) => {
-    if (start === undefined) {
-      start = timestamp;
+    function raf(time) {
+      lenis.raf(time);
+
+      requestAnimationFrame(raf);
     }
-    const elapsed = timestamp - start;
-    loader.current.style.top =
-      easeOutQuad(elapsed, 0, -loaderHeight(), duration) + "px";
 
-    if (elapsed < duration) {
-      requestAnimationFrame(animate);
-    }
-  };
-
-  const easeOutQuad = (time, start, end, duration) => {
-    return -end * (time /= duration) * (time - 2) + start;
-  };
-
-  const loaderHeight = () => {
-    const loaderBounds = loader.current.getBoundingClientRect();
-    return loaderBounds.height;
-  };
-
-  const setPath = (curve) => {
-    const width = window.innerWidth;
-    const height = loaderHeight();
-    path.current.setAttributeNS(
-      null,
-      "d",
-      `M0 0
-    L${width} 0
-    L${width} ${height}
-    Q${width / 2} ${height - curve} 0 ${height}
-    L0 0`
-    );
-  };
+    requestAnimationFrame(raf);
+  });
 
   return (
     <>
-      <div ref={loader} className="loader w-full fixed z-10">
-        <svg className="h-full w-full">
-          <path
-            ref={path}
-            className="stroke-[#f1cf29] stroke-1 fill-[#f1cf29]"
-          ></path>
-        </svg>
-      </div>
+      <Loader />
       <Header />
       <main className="h-full w-full flex-1">
         <Hero />
